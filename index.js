2ebf3c1c75a0ef028569836b8d2bd963710275ac
SharunTawsif <sharuntawsif1@gmail.com>
2026-01-01 11:47:49 +0600

    Active class added for permanent hover

--- index.js
+++ index.js
@@ -18,13 +18,84 @@ const displayCategories = (categories) => {
     for (const category of categories) {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-            <button onclick="loadCategoryElement('${category.id}')" class="text-[#1F2937]/80 text-left text-medium">${category.category_name}</button>
+            <button id = "category-btn-${category.id}" onclick="loadCategoryElement('${category.id}')" class="text-[#1F2937]/80 text-left text-medium hover:bg-[#15803D] p-3 rounded-md flagg">${category.category_name}</button>
         `;
         categoryContainer.append(btnDiv);
     }
 
 }
 // Category loading end
+
+const removeActive = () => {
+    const categoryButton = document.querySelectorAll(".flagg") //agee dot disi karon css er moto kore dhorte hoy 
+    for (const ctBtn of categoryButton) {
+        ctBtn.classList.remove("active");
+    }
+}
+
+
+
+// load all plant start
+
+const allPlant = () => {
+    fetch("https://openapi.programming-hero.com/api/plants")
+    .then(res => res.json())
+    .then(data => displayAllPlants(data.plants))
+}
+
+allPlant();
+
+const displayAllPlants = (datas) => {
+    const treeContainer = document.getElementById("tree-container")
+    treeContainer.innerHTML =""
+
+    for (const data of datas) {
+        // console.log(element);
+        const card = document.createElement("div")
+        card.innerHTML = `
+            <div
+                class="bg-white text-black rounded-xl shadow-sm p-5 flex flex-col h-full"
+                >
+                <img src="${data.image}" alt="" class="h-40 object-cover rounded-md" />
+
+                <p class="text-left font-bold text-lg mt-3">
+                    ${data.name}
+                </p>
+
+                <p class="text-left text-[#71717A]/80 flex-grow">
+                    ${data.description}
+                </p>
+
+                <div class="flex justify-between items-center w-full mt-3">
+                    <p class="inline-block bg-[#DCFCE7] rounded-2xl p-3 text-[#15803D]">
+                    ${data.category}
+                    </p>
+                    <p class="font-semibold">৳${data.price}</p>
+                </div>
+
+                <button class="bg-[#15803D] rounded-3xl text-white p-3 w-full mt-4">
+                    Add to Cart
+                </button>
+            </div>
+
+        `;
+        treeContainer.append(card);
+
+    }
+}
+
+
+
+
+
+
+// load all plant end
+
+
+
+
+
+
 
 
 
@@ -34,7 +105,13 @@ const loadCategoryElement = (categoryId) => {
     const url = `https://openapi.programming-hero.com/api/category/${categoryId}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => displayCategoryElement(data.plants))
+    .then(data => {
+        removeActive();
+        const clickBtn = document.getElementById(`category-btn-${categoryId}`)
+        clickBtn.classList.add("active")
+        displayCategoryElement(data.plants)
+
+    })
 
 }
 
@@ -61,7 +138,7 @@ const displayCategoryElement = (elements) => {
                 </p>
 
                 <div class="flex justify-between items-center w-full mt-3">
-                    <p class="inline-block bg-[#DCFCE7] rounded-xl px-2 py-1">
+                    <p class="inline-block bg-[#DCFCE7] rounded-2xl p-3 text-[#15803D]">
                     ${element.category}
                     </p>
                     <p class="font-semibold">৳${element.price}</p>
